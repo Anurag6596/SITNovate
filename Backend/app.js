51650437026824293fb647bea4a86b20eb018340
ianuraggavhane <77688394+Anurag6596@users.noreply.github.com>
2025-02-19 20:55:34 +0530

    creating the usermodel ,usercontrollers ,userRoutes,services

--- Backend/app.js
+++ Backend/app.js
@@ -4,12 +4,14 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const connectDB = require('./db/db');
+const userRoutes = require('../Backend/routes/user.routes');
 
 connectDB();
 
 
-
 app.use(cors());
+
+app.use('/user',userRoutes);
 
 app.get('/', (req, res) => {
     res.send('Hello World');
