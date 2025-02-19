6cd54a01beb4924dc20dc5a0c71b8e1c477de63c
ianuraggavhane <77688394+Anurag6596@users.noreply.github.com>
2025-02-20 02:56:38 +0530

    adding payment gateway

--- Backend/server.js
+++ Backend/server.js
@@ -1,13 +1,13 @@
 const http = require('http');
 const app = require('./app');
 const port = process.env.PORT || 4000;
-
+const express = require('express');
 
 
 
 const server = http.createServer(app);
 
-
+app.use("/payment", require("./routes/payment"));
 
 server.listen(port, () => { 
     console.log(`Server is running on port ${port}`);
